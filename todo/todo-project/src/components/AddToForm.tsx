1ce6cdc1b6f0ba034071d902d1c24531d385f822
Gym Bigogwe <gymbigogwe@Bigogwes-iMac.local>
2025-08-08 14:22:24 +0200

    addedsome function to help to handle form of inputting a todo tittle

--- todo/todo-project/src/components/AddToForm.tsx
+++ todo/todo-project/src/components/AddToForm.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddToForm = () => {
+    const [input,setinput]=useState("")
+    interface Adding{
+        
+    }
+
+     function handlesubmitt(e:React.FormEvent<HTMLFormElement>){
+  e.preventDefault()
+  if(!input.trim()) return ;
+     }
+
   return (
-   <form action="">
-    <input type="text" placeholder='What needs to be done!' className='rounded-s-md grow border border-gray-50-400 p-2'/>
+   <form action="" onSubmit={handlesubmitt}>
+    <input type="text" value={input} onChange={(e)=>setinput(e.target.value)} placeholder='What needs to be done!' className='rounded-s-md grow border border-gray-50-400 p-2'/>
+    <button type='submit' className='bg-white-500 text-blue-600'>+</button>
    </form>
   )
 }
